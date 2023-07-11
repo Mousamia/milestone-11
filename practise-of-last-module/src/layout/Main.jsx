25153510e636acd1de8822e29b37a030aa4dcf29
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-07-11 23:28:58 +0600

    config revised

---
+++ practise-of-last-module/src/layout/Main.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            Main
+            
+        </div>
+    );
+};
+
+export default Main;
--- practise-of-last-module/src/main.jsx
+++ practise-of-last-module/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Main from './layout/Main';
+import Home from './components/Home/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
